38d7ff1bcfca08f4712edd402a7bf7602bb96766
Vuk Bojanic <vukgraphicart@gmail.com>
2017-09-27 14:50:57 +0200

    Added particles js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('particles.json loaded...')
+});
+
 $(document).ready(function() {
   $('.vuk-container').delay(1500).fadeIn(300);
   });
